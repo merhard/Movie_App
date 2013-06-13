3499f09603420d4bd403233a0d8510dca82f6c4c
Matthew Erhard <merhard@gmail.com>
2013-06-13 16:33:45 -0400

    initial commit
    rspec, capybara, postgres installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoviesApp::Application.config.secret_token = '56f1c313690bae8181fa663a6f16cce33a97e0c4da2ae2375f1c0b52620213914d3cdf6ebe09d9ff40f1744aaf415622412ab2ae8ac2fc5f1e55c9124418c9f0'
